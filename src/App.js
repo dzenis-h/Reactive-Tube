3e99dbd9063d7e0c6f7c06c4a4abfb78ec758db0
Dzenis H <dzenis.hankusic@gmail.com>
2019-02-26 02:58:43 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import YTSearch from "youtube-api-search";
 import SearchBar from "./components/search_bar";
 import VideoList from "./components/video_list";
 import VideoDetail from "./components/video_detail";
-import API_KEY from "./config/api_key";
+import API_KEY from "./config/key_prod";
 
 
 class App extends Component {
